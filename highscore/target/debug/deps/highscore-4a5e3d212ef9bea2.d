3890be565ff648fa82fc60e7939780a820a51c84
Bavenraj <bavenraj08@gmail.com>
2024-07-11 09:11:11 +0800

    second exercise

---
+++ highscore/target/debug/deps/highscore-4a5e3d212ef9bea2.d
@@ -0,0 +1,5 @@
+C:\Users\bavenraj.partipan\OneDrive - Tune Protect Group\Desktop\exercism_rust\highscore\target\debug\deps\libhighscore-4a5e3d212ef9bea2.rmeta: src/main.rs
+
+C:\Users\bavenraj.partipan\OneDrive - Tune Protect Group\Desktop\exercism_rust\highscore\target\debug\deps\highscore-4a5e3d212ef9bea2.d: src/main.rs
+
+src/main.rs:
---
+++ highscore/target/debug/deps/highscore-ff5e3270f52284a7.d
@@ -0,0 +1,5 @@
+C:\Users\bavenraj.partipan\OneDrive - Tune Protect Group\Desktop\exercism_rust\highscore\target\debug\deps\libhighscore-ff5e3270f52284a7.rmeta: src/main.rs
+
+C:\Users\bavenraj.partipan\OneDrive - Tune Protect Group\Desktop\exercism_rust\highscore\target\debug\deps\highscore-ff5e3270f52284a7.d: src/main.rs
+
+src/main.rs:
