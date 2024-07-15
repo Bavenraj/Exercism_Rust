04b14ca4e0de851f9e9d4e4971b9765635f81e69
Bavenraj <bavenraj08@gmail.com>
2024-07-15 09:31:29 +0800

    added comment

--- highscore/src/main.rs
+++ highscore/src/main.rs
@@ -25,6 +25,7 @@ impl HighScores {
     pub fn personal_top_three(&self) -> Vec<u32> {
         // had to clone since it was a vector and it takes ownership
         let mut score_copy = self.scores.clone();
+        // sort it descending
         score_copy.sort();
         //copy the iterable and reverse it. from this list, retrieve the first three values
         score_copy.iter().copied().rev().take(3).collect()
