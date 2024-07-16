40a3c0aac66061f8b086e0822c58b8830ed4dbce
Bavenraj <bavenraj08@gmail.com>
2024-07-16 13:39:31 +0800

    add

--- highscore/src/main.rs
+++ highscore/src/main.rs
@@ -1,25 +1,25 @@
 #[derive(Debug)]
 pub struct HighScores {
-    scores: Vec<u32>
+    score: Vec<u32>
 }
 impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
+    pub fn new(score: &[u32]) -> Self {
         HighScores {
-            scores: Vec::from(scores)
+            score: Vec::from(score)
         }
     }
     pub fn scores(&self) -> &[u32] {
-        &self.scores[..]
+        &self.score[..]
     }
     pub fn latest(&self) -> Option<u32> {
-        self.scores.last().copied()
+        self.score.last().copied()
     }
     pub fn personal_best(&self) -> Option<u32> {
-        self.scores.iter().max().copied()
+        self.score.iter().max().copied()
     }
     pub fn personal_top_three(&self) -> Vec<u32> {
         // had to clone since it was a vector and it takes ownership
-        let mut score_copy = self.scores.clone();
+        let mut score_copy = self.score.clone();
         // sort it descending
         score_copy.sort();
         //copy the iterable and reverse it. from this list, retrieve the first three values
