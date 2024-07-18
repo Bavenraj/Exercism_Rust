1fe7665dd310f704a9c082cec8ab92a4b25a93fb
Bavenraj <bavenraj08@gmail.com>
2024-07-18 16:40:53 +0800

    added

--- highscore/src/main.rs
+++ highscore/src/main.rs
@@ -5,7 +5,7 @@ pub struct HighScores {
 impl HighScores {
     pub fn new(score: &[u32]) -> Self {
         HighScores {
-            score: Vec::from()
+            score: Vec::from(score)
         }
     }
     pub fn scores(&self) -> &[u32] {
