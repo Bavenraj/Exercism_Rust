51222b4f5f79eb937191dbeb65ce71d10097d2b2
BAVENRAJ <bavenraj08@gmail.com>
2024-07-20 22:48:34 +0800

    exercise

--- sum_multiples/src/main.rs
+++ sum_multiples/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    let mut sum: u32 = 0;
+    let mut intermediate_result: Vec<u32> = Vec::new();
+
+    for &factor in factors {
+        if factor == 0 {
+            continue; 
+        }
+
+        let mut i = 1;
+        while i * factor < limit {
+            let multiple = i * factor;
+            if !intermediate_result.contains(&multiple) {
+                intermediate_result.push(multiple);
+            }
+            i += 1;
+        }
+    }
+
+    for result in intermediate_result {
+        sum += result;
+    }
+
+    sum    
 }
