85539c3e4cff7ceee7663b9aff75792c47192ef3
BAVENRAJ <bavenraj08@gmail.com>
2024-07-20 22:47:36 +0800

    exercise

--- sumsquare/target/debug/deps/execism-84a10039a96381ba.d
+++ sumsquare/target/debug/deps/execism-84a10039a96381ba.d
@@ -1,5 +1,5 @@
-C:\Users\bavenraj.partipan\OneDrive - Tune Protect Group\Desktop\exercism\target\debug\deps\libexecism-84a10039a96381ba.rmeta: src/main.rs
+C:\Users\LENOVO\Downloads\exercism\sumsquare\target\debug\deps\libexecism-84a10039a96381ba.rmeta: src/main.rs
 
-C:\Users\bavenraj.partipan\OneDrive - Tune Protect Group\Desktop\exercism\target\debug\deps\execism-84a10039a96381ba.d: src/main.rs
+C:\Users\LENOVO\Downloads\exercism\sumsquare\target\debug\deps\execism-84a10039a96381ba.d: src/main.rs
 
 src/main.rs:
