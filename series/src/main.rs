4e522102833d7ace2a3dd40a28f3dbf63317934c
BAVENRAJ <bavenraj08@gmail.com>
2024-07-28 22:14:43 +0800

    test

--- series/src/main.rs
+++ series/src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::fmt;
 
 pub fn series(digits: &str, len: usize) -> Vec<String> {
     let mut list: Vec<String> = Vec::new();
@@ -8,7 +8,7 @@ pub fn series(digits: &str, len: usize) -> Vec<String> {
     let mut left = 0;
     let mut right = len - 1;
     while right < digits.len() {
-        list.push(digits[left..=ight].to_string());
+        list.push(digits[left..=right].to_string());
         left += 1;
         right = left + len - 1;
     }
